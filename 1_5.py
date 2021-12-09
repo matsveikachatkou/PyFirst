6de0a1cf78ad0ab5a4d5f39304c66750700d1158
matsveikachatkou <allthesammy@gmail.com>
2021-12-09 14:53:33 +0100

    1_5

--- 1_5.py
+++ 1_5.py
@@ -4,8 +4,7 @@ costs = int(input("Please provide costs: "))
 if revenue >= costs:
     print(f"Your Net Profit is {revenue - costs}$")
     print(f"Profitability is {(revenue - costs)/revenue:.2%}")
+    number_employers = int(input("Please provide number of employers: "))
+    print(f"Net Profit per cap: {(revenue - costs) / number_employers}$")
 else:
     print(f"Your Loss is {costs - revenue}$")
-
-number_employers = int(input("Please provide number of employers: "))
-print(f"Net Profit/Loss per cap: {(revenue - costs)/number_employers}$")
