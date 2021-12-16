5b5d1be7a63b6679e2663b0025f953a55d4024d2
matsveikachatkou <allthesammy@gmail.com>
2021-12-16 16:47:58 +0100

    2_2

---
+++ 2_2.py
@@ -0,0 +1,27 @@
+number_of_elements = int(input("How many elements do you want to provide? "))
+if number_of_elements < 0:
+    number_of_elements = 0
+list_of_elements = []
+counter = 0
+
+while counter < number_of_elements:
+    element_of_the_list = input("Please provide element of the list: ")
+    list_of_elements.append(element_of_the_list)
+    counter += 1
+
+new_list = []
+
+if len(list_of_elements) % 2 == 0:
+    for index in range(0, len(list_of_elements), 2):
+        new_list.append(list_of_elements[index+1])
+        new_list.append(list_of_elements[index])
+elif len(list_of_elements) == 1:
+    new_list = list_of_elements
+else:
+    for index in range(0, len(list_of_elements)-1, 2):
+        new_list.append(list_of_elements[index + 1])
+        new_list.append(list_of_elements[index])
+    new_list.append(list_of_elements[-1])
+
+
+print(new_list)
