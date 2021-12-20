98c0afd9ced8f47532c3c49295bc16c62b89458d
matsveikachatkou <allthesammy@gmail.com>
2021-12-20 15:18:10 +0100

    3_2

---
+++ 3_3.py
@@ -0,0 +1,11 @@
+def my_func(one, two, three):
+    list_of_values = [one, two, three]
+    for element in list_of_values:
+        if type(element) != int and type(element) != float:
+            return
+    list_of_values.sort(reverse=True)
+    return list_of_values[0] + list_of_values[1]
+
+
+print(my_func("adf", 5, 6))
+print(my_func(11, 5, 6))
