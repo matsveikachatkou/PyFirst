7b7ec26d6fb706ef1586b05b68021fb1542713af
matsveikachatkou <allthesammy@gmail.com>
2021-12-23 17:03:26 +0100

    4_4

--- 4_4.py
+++ 4_4.py
@@ -1,4 +1,4 @@
-list_of_numbers = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+list_of_numbers = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 111]
 
 dict_numbers = {number: 0 for number in list_of_numbers}
 for number in list_of_numbers:
