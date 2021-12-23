e96f5ab9b67c0e6dffea93c419347e36a2a9d72d
matsveikachatkou <allthesammy@gmail.com>
2021-12-23 16:25:33 +0100

    4_1

--- 4_1.py
+++ 4_1.py
@@ -1,4 +1,12 @@
 from sys import argv
 
-script_name, hours_worked, hour_wage, bonus = argv
-print(f'Your salary is {float(hour_wage) * float(hours_worked) + float(bonus)}$')
+
+def salary():
+    try:
+        hours_worked, hour_wage, bonus = argv[1:]
+        print(f'Your salary is {float(hour_wage) * float(hours_worked) + float(bonus)}$')
+    except ValueError:
+        print("Please enter numbers!")
+
+
+salary()
